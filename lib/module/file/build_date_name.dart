43eaa74519a6d02ec3e1264040d822513627c6d2
e.kubeckiy <e.kubeckiy@sports.ru>
2023-12-19 10:09:48 +0300

    fix: refactor func & test

--- lib/module/file/build_date_name.dart
+++ lib/module/file/build_date_name.dart
@@ -1,3 +1,3 @@
 String buildDateName(DateTime dateTime) {
-  return '${dateTime.toIso8601String().substring(0, 10)}_${DateTime.now().millisecondsSinceEpoch.toString().substring(6)}';
+  return '${dateTime.toIso8601String().substring(0, 10)}_${DateTime.now().millisecondsSinceEpoch.toString().substring(9)}';
 }
--- test/module/file/build_date_name_test.dart
+++ test/module/file/build_date_name_test.dart
@@ -4,8 +4,8 @@ import 'package:test/test.dart';
 void main() {
   group('buildDateName', () {
     test('should build string from date', () async {
-      expect(buildDateName(DateTime(2023, 07, 23)),
-          '2023-07-23_${DateTime.now().millisecondsSinceEpoch.toString().substring(6)}');
+      expect(
+          buildDateName(DateTime(2023, 07, 23)).substring(0, 10), '2023-07-23');
     });
   });
 }
